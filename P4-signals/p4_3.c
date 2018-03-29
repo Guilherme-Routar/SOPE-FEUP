f627c718c65ba81345a8bc4410e88bba39f5518a
Guilherme-Routar <groutar@gmail.com>
2018-03-29 15:51:44 +0100

    ex3 completed with error checking and all

--- P4-signals/p4_3.c
+++ P4-signals/p4_3.c
@@ -2,37 +2,71 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
 
 int var = 0;
 int incFlag = 1;
 
 void sigint_handler(int signo) {
-    if (signo == SIGUSR1)
+    if (signo == SIGUSR1) {
         incFlag = 1;
-    if (signo == SIGUSR2)
+        printf("SIGUSR1 triggered - Increment var\n");
+    }
+    if (signo == SIGUSR2) {
         incFlag = 0;
+        printf("SIGUSR2 triggered - Decrement var\n");
+    }
 }
 
 int main(void) {
 
-    // a)
-    /*
-    struct sigaction action;
-    action.sa_handler = sigint_handler;
-    sigemptyset(&action.sa_mask);
-    action.sa_flags = 0;
+    int status;
+    pid_t pid;
+    pid = fork();
 
-    sigaction(SIGUSR1, &action, NULL);
-    sigaction(SIGUSR2, &action, NULL);
-
-    while(1) {
-        printf("%d\n", var);
-        if (incFlag == 1)
-            var++;
-        else var--;
-        sleep(1);
+    if (pid > 0) {
+        printf("Dad launched with pid %d\n", getpid());
+        
+        int signal = 1, sleepTime;
+        for (int i = 0; i < 10; i++) {
+            sleepTime = rand() % 10;
+            sleep(sleepTime);
+            if (signal == 1) {
+                kill(pid, SIGUSR1);
+                signal = 2;
+            }
+            else {
+                kill(pid, SIGUSR2);
+                signal = 1;
+            }
+        }
+        wait(&status);
     }
-    */
+    else if (pid == 0) {
+        printf("Child launched with pid %d\n", getpid());
+
+        struct sigaction action;
+        action.sa_handler = sigint_handler;
+        sigemptyset(&action.sa_mask);
+        action.sa_flags = 0;
+
+        if (sigaction(SIGUSR1, &action, NULL) < 0)
+            perror("Failed to execute SIGUSR1 handler");
+        if (sigaction(SIGUSR2, &action, NULL) < 0)
+            perror("Failed to execute SIGUSR2 handler");
+        
+        for (int i = 0; i < 50; i++) {
+            if (incFlag == 1) var++;
+            else var--;
+            printf("Var = %d\n", var);
+            sleep(1);
+        }
+        exit(0);
+    }
+    else {
+        perror("Fork failed");
+    }
 
     exit(0);
 }
