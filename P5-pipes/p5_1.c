3c51a2256edd4c762eb50b1e312f4efe2a2eed95
Guilherme-Routar <groutar@gmail.com>
2018-05-02 14:45:57 +0100

    finished ex1

--- P5-pipes/p5_1.c
+++ P5-pipes/p5_1.c
@@ -4,7 +4,14 @@
 #define READ 0
 #define WRITE 1
 
-int main(int argc, char * argv[]) {
+struct numbers
+{
+    int n1;
+    int n2;
+};
+
+int main(int argc, char *argv[])
+{
 
     pid_t pid;
     int fd[2];
@@ -14,18 +21,18 @@ int main(int argc, char * argv[]) {
 
     if (pid > 0) //dad
     {
-        int n[2];
-        scanf("%d %d", &n[0], &n[1]);
+        struct numbers n;
+        scanf("%d %d", &n.n1, &n.n2);
         close(fd[READ]);
-        write(fd[WRITE], n, 2*sizeof(int));
+        write(fd[WRITE], &n, 2 * sizeof(int));
         close(fd[WRITE]);
     }
     else if (pid == 0) //child
     {
+        struct numbers n;
         close(fd[WRITE]);
-        int n[2];
-        read(fd[READ], n, 2*sizeof(int));
-        printf("%d", n[0] + n[1]);
+        read(fd[READ], &n, 2 * sizeof(int));
+        printf("%d", n.n1 + n.n2);
         close(fd[READ]);
     }
 
