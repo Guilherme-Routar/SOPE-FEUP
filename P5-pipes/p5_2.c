19a5f9ec302b6a09405081656dd92e93f053e597
Guilherme-Routar <groutar@gmail.com>
2018-05-02 15:50:44 +0100

    ex2 almost done, result not being updated for some reason

---
+++ P5-pipes/p5_2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define READ 0
+#define WRITE 1
+
+struct numbers
+{
+    int n1;
+    int n2;
+};
+
+int main(int argc, char *argv[])
+{
+
+    pid_t pid;
+    int fd[2];
+    int fd2[2];
+
+    pipe(fd);
+    pid = fork();
+
+    if (pid > 0) //dad
+    {
+        struct numbers n;
+        printf("x y ? ");
+        scanf("%d %d", &n.n1, &n.n2);
+        close(fd[READ]);
+        write(fd[WRITE], &n, 2 * sizeof(int));
+        close(fd[WRITE]);
+
+        int result = 0;
+        close(fd2[WRITE]);
+        read(fd2[READ], &result, sizeof(int));
+        printf("result = %d", result);
+        close(fd2[READ]);
+    }
+    else if (pid == 0) //child
+    {
+        struct numbers n;
+        close(fd[WRITE]);
+        read(fd[READ], &n, 2 * sizeof(int));
+        int result = n.n1 + n.n2;
+        close(fd[READ]);
+
+        close(fd2[READ]);
+        write(fd2[WRITE], &result, sizeof(int));
+        close(fd2[WRITE]);
+    }
+
+    return 0;
+}
