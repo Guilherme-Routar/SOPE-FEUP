8806b6c562334bd9b167473f518a12413629df35
Guilherme-Routar <groutar@gmail.com>
2018-05-14 21:48:36 +0100

    Adde whole logic - missing request error checkign

--- Final Project/server.c
+++ Final Project/server.c
@@ -23,6 +23,7 @@ Seat *init_seats_list();
 void launch_ticket_offices_threads(int num_ticket_offices);
 void *ticket_office_handler(void *arg);
 
+void free_booked_seats(int booked_seats[], int size);
 int isSeatFree(Seat *seat, int seatNum);
 void bookSeat(Seat *seat, int seatNum, int clientId);
 void freeSeat(Seat *seat, int seatNum);
@@ -161,87 +162,40 @@ void *ticket_office_handler(void *arg)
   {
     sem_wait(&full);
     myreq = request;
+    bookSeat(&seats[0], 1, 9999);
     sem_post(&empty);
 
-    //printf("return = %d\n", validate_request(myreq));
     int request_status = validate_request(myreq);
-
     switch (request_status)
     {
     case VALID_REQUEST:
     {
-      int availability = check_room_availability();
-      int booked_seats_counter = 0;
-      int booked_seats[myreq.num_wanted_seats];
-      //booked_seats = init_booked_seats_list(booked_seats);
-      if (availability == AVAILABLE_SEATS)
-      {
-        for (int i = 0; i < myreq.pref_seats_size; i++)
-        {
-          for (int j = 0; j < num_room_seats; j++)
-          {
-            if (seats[j].number == myreq.pref_seat_list[i])
-            {
-              if (isSeatFree(&seats[j], myreq.pref_seat_list[i]) == SEAT_AVAILABLE)
-              {
-                if (booked_seats_counter < myreq.num_wanted_seats)
-                {
-                  printf("Trying to book seat #%d\n", myreq.pref_seat_list[i]);
-                  bookSeat(&seats[j], myreq.pref_seat_list[i], myreq.pid);
-                  booked_seats[booked_seats_counter] = myreq.pref_seat_list[i];
-                  booked_seats_counter++;
-                }
-              }
+      int booked_seats_list[myreq.num_wanted_seats]; // Initialized to -1
+      int booked_seats = 0;
+      // try to book seats
+      for (int i = 0; i < myreq.pref_seats_size; i++) {
+        for (int j = 0; j < num_room_seats; j++) {
+          if (myreq.pref_seat_list[i] == seats[j].number) {
+            if (isSeatFree(&seats[j], myreq.pref_seat_list[i])) {
+              bookSeat(&seats[j], myreq.pref_seat_list[i], myreq.pid);
+              booked_seats_list[booked_seats] = myreq.pref_seat_list[i];
+              booked_seats++;
             }
           }
         }
-        if (booked_seats_counter == myreq.num_wanted_seats)
-        {
-          printf("Seats successfully booked\n");
-          for (int i = 0; i < myreq.num_wanted_seats; i++)
-          {
-            printf("Booked seat: #%d\n", booked_seats[i]);
+      }
+      if (booked_seats == myreq.num_wanted_seats)
+        printf("Successful reservation");
+      else {
+        for (int i = 0; i < num_room_seats; i++) {
+          for (int j = 0; j < myreq.num_wanted_seats; j++) {
+            if (seats[i].number == booked_seats_list[j]) {
+              freeSeat(&seats[i], booked_seats_list[j]);
+            }
           }
+        }
+      }
 
-          for (int i = 0; i < num_room_seats; i++)
-          {
-            printf("Seat # %d is av: %d\n", seats[i].number, seats[i].available);
-          }
-        }
-        else
-        {
-          printf("Unable to make reservation");
-          for (int i = 0; i < myreq.num_wanted_seats; i++)
-          {
-            freeSeat(&booked_seats[i], booked_seats[i].number);
-          }
-          fprintf(fslog, "%d-%d-%d: list of seats - %s\n", pthread_self(), myreq.pid, myreq.num_wanted_seats, "NAV");
-        }
-      }
-      else if (availability == FULL_ROOM)
-      {
-        fprintf(fslog, "%d-%d-%d: list of seats - %s\n", pthread_self(), myreq.pid, myreq.num_wanted_seats, "FUL");
-      }
-      break;
-    }
-    case INVALID_PARAMETERS:
-    {
-      fprintf(fslog, "%d-%d-%d: list of seats - %s\n", (int)pthread_self(), myreq.pid, myreq.num_wanted_seats, "ERR");
-      break;
-    }
-    case OVERFLOW_NUM_WANTED_SEATS:
-    {
-      fprintf(fslog, "%d-%d-%d: list of seats - %s\n", (int)pthread_self(), myreq.pid, myreq.num_wanted_seats, "MAX");
-      break;
-    }
-    case INVALID_NUMBER_PREF_SEATS:
-    {
-      fprintf(fslog, "%d-%d-%d: list of seats - %s\n", (int)pthread_self(), myreq.pid, myreq.num_wanted_seats, "NST");
-      break;
-    }
-    case INVALID_SEAT_NUMBER:
-    {
-      fprintf(fslog, "%d-%d-%d: list of seats - %s\n", (int)pthread_self(), myreq.pid, myreq.num_wanted_seats, "IID");
       break;
     }
     }
@@ -250,8 +204,23 @@ void *ticket_office_handler(void *arg)
   fprintf(fslog, "%d-CLOSED\n", (int)pthread_self());
 }
 
-void init_booked_seats_list()
+void free_booked_seats(int booked_seats[], int size)
 {
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < num_room_seats; j++)
+    {
+      if (booked_seats[i] == seats[j].number)
+      {
+        freeSeat(&seats[j], booked_seats[i]);
+      }
+    }
+  }
+
+  for (int i = 0; i < num_room_seats; i++)
+  {
+    printf("After freeing Seat # %d is av: %d\n", seats[i].number, seats[i].available);
+  }
 }
 
 int check_room_availability()
@@ -275,13 +244,10 @@ Seat *init_seats_list()
   Seat s;
   for (int i = 0; i < num_room_seats; i++)
   {
-    if (i == 0) {
-      s.number = i + 1;
-      s.available = SEAT_AVAILABLE;
-      s.client_id = -1;
-      seats[i] = s;
-    }
-    
+    s.number = i + 1;
+    s.available = SEAT_AVAILABLE;
+    s.client_id = -1;
+    seats[i] = s;
   }
 
   return seats;
@@ -290,29 +256,16 @@ Seat *init_seats_list()
 // Testing if seat seatNum is free
 int isSeatFree(Seat *seat, int seatNum)
 {
-  /*
-  for (int i = 0; i < num_room_seats; i++)
-  {
-      printf("Seat # %d is av: %d\n", seats[i].number, seats[i].available);
-  }
-  */
-  Seat s = *seat;
-  printf("Checking if seat #%d is free\n", s.number);
-  if (s.available == SEAT_AVAILABLE)
-  {
-    printf("Seat %d available\n", s.number);
+  if ((*seat).available == SEAT_AVAILABLE)
     return SEAT_AVAILABLE;
-  }
   else
-  {
-    printf("Seat %d NOT available\n", s.number);
     return SEAT_UNAVAILABLE;
-  }
 }
 
 // Booking seat seatnum for client clientId
 void bookSeat(Seat *seat, int seatNum, int clientId)
 {
+  //printf("Booking seat #%d\n", (*seat).number);
   (*seat).available = SEAT_UNAVAILABLE;
   (*seat).client_id = clientId;
 }
@@ -320,6 +273,7 @@ void bookSeat(Seat *seat, int seatNum, int clientId)
 // Freeing seat seatNum (in case the final reservation couldn't be done)
 void freeSeat(Seat *seat, int seatNum)
 {
+  printf("Freeing seat #%d\n", (*seat).number);
   (*seat).available = SEAT_AVAILABLE;
   (*seat).client_id = -1;
 }
